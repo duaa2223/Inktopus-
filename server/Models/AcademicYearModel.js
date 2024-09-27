8d24766adf0c959eda265b511526f5b9c8bffa74
Dua'a Khamis <your_email@example.com>
2024-09-27 03:11:00 +0300

    push

--- server/Models/AcademicYearModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const academicYearSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }
-});
-
-const AcademicYear = mongoose.model('AcademicYear', academicYearSchema);
-
-module.exports = AcademicYear;
--- server/Routes/ResourceRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { getAllColleges } = require('../Controllers/ResourceController'); 
-
-// الحصول على جميع الكليات
-router.get('/colleges', getAllColleges);
-
-module.exports = router;
