2facee28ec423f180852c36e4106dcfb6f748d6f
Dua'a Khamis <your_email@example.com>
2024-11-04 13:07:20 +0300

    push

--- server/Models/SpecializationModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SpecializationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  college: { type: mongoose.Schema.Types.ObjectId, ref: 'College', required: true },
-  academic_year: { type: mongoose.Schema.Types.ObjectId, ref: 'AcademicYear', required: true },
-});
-
-module.exports = mongoose.model('Specialization', SpecializationSchema);
